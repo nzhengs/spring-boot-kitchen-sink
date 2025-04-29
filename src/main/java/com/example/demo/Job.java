0028d54bdf05678dafc7b7472f787961bf651216
anjanahengaju <45703218+nzhengs@users.noreply.github.com>
2025-04-28 23:11:10 -0400

    Add H2.

--- src/main/java/com/example/demo/Job.java
+++ src/main/java/com/example/demo/Job.java
@@ -1,8 +1,16 @@
 package com.example.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.time.LocalDate;
 
+@Entity
 public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public Integer id;
     public String companyName;
     public String role;
--- src/main/java/com/example/demo/JobController.java
+++ src/main/java/com/example/demo/JobController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,25 +13,15 @@ import java.util.List;
 @RequestMapping("/jobs")
 public class JobController {
 
+    private final JobRepository jobRepository;
+
+    public JobController(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+
     @GetMapping("/")
-    public List<Job> index() {
-        Job job1 = new Job();
-        job1.id = 123;
-        job1.companyName = "company 1";
-        job1.appliedDate = LocalDate.now();
-        job1.status=JobStatus.IN_PROGRESS;
-
-
-        Job job12 = new Job();
-        job12.id = 12223;
-        job12.companyName = "company 22";
-        job12.appliedDate = LocalDate.now();
-        job12.status=JobStatus.NOT_STARTED;
-
-        ArrayList<Job> jobs = new ArrayList<>();
-
-        jobs.add(job1);
-        jobs.add(job12);
-        return jobs;
+    public Iterable<Job> index() {
+        return jobRepository.findAll();
     }
 }
